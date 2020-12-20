590c15f2b852a21c2bb3ac38a752cb468552af5b
Beatriz Milz <beatriz.milz@hotmail.com>
2020-12-20 18:42:04 -0300

    atualiza a apresentação

--- R/hexstickerwall.R
+++ R/hexstickerwall.R
@@ -1,6 +1,6 @@
 source("R/hexwall.R")
 logos <- hexwall("docs/img/logos/",
-                 sticker_row_size = 5,
+                 sticker_row_size = 8,
                  sticker_width = 200)
 
 logos
